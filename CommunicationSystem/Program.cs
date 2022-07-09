654178a2740983699b9b850d56da2bc3b843b2ad
Nikita <nik.lizard.mobile@gmail.com>
2022-07-09 12:46:02 +0400

    Delete unnecessary files

--- CommunicationSystem/Program.cs
+++ CommunicationSystem/Program.cs
@@ -2,8 +2,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Serilog;
-using Serilog.Events;
-using Serilog.Filters;
 using System;
 
 namespace CommunicationSystem
@@ -36,10 +34,10 @@ namespace CommunicationSystem
             Host.CreateDefaultBuilder(args)
                 .ConfigureAppConfiguration((context, builder) =>
                 {
+                    builder.AddJsonFile("logging.json");
                     if (context.HostingEnvironment.IsDevelopment())
                     {
                         builder.AddUserSecrets<Program>();
-                        builder.AddJsonFile("logging.json");
                     }
                 })
             .UseSerilog((context, services, configuration) =>
