24c1a782a67bae7b8a37812017fbbca4f1a4a178
Nikita <nik.lizard.mobile@gmail.com>
2023-01-16 18:09:12 +0400

    Change PeerJs-Server address to connect to official server

--- CommunicationSystem/ClientApp/src/environments/environment.prod.ts
+++ CommunicationSystem/ClientApp/src/environments/environment.prod.ts
@@ -1,10 +1,6 @@
 export const environment = {
   production: true,
   whiteListedHosts: ["localhost:5001", "192.168.64.21"],
-  //peerServer: "e-order.ru",
-  //peerServerPort: 9000,
-  //peerServer: "0.peerjs.com",
-  //peerServerPort: 443
-  peerServer: "peerjs-server.herokuapp.com",
+  peerServer: "1.peerjs.com",
   peerServerPort: 443
 };
